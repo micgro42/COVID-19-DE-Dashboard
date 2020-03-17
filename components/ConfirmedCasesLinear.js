3996a23be91a2b6f1a9ed0b2c34196659260889d
Michael Große <mic.grosse@googlemail.com>
2020-03-17 09:26:07 +0100

    Add remaining states to Proof of Concept chart
    Obviously, this will come from the store soon.

--- components/ConfirmedCasesLinear.js
+++ components/ConfirmedCasesLinear.js
@@ -22,7 +22,7 @@ const ConfirmedCasesLinear = () => {
 
   return (
     <div>
-      <p>Confirmed Cases in Saxony and Berlin</p>
+      <p>Confirmed Cases</p>
       <LineChart
         height={400}
         width={1200}
@@ -33,8 +33,22 @@ const ConfirmedCasesLinear = () => {
         <YAxis />
         <Tooltip />
         <CartesianGrid stroke="#f5f5f5" />
-        <Line type="monotone" dataKey="Sachsen" stroke="#ff7300" />
-        <Line type="monotone" dataKey="Berlin" stroke="#387908" />
+        <Line type="monotone" dataKey="Baden-Württemberg" stroke="hsla(0, 100%, 50%, 1)" />
+        <Line type="monotone" dataKey="Bayen" stroke="hsla(22, 100%, 50%, 1)" />
+        <Line type="monotone" dataKey="Berlin" stroke="hsla(45, 100%, 50%, 1)" />
+        <Line type="monotone" dataKey="Brandenbug" stroke="hsla(67, 100%, 50%, 1)" />
+        <Line type="monotone" dataKey="Bremen" stroke="hsla(90, 100%, 50%, 1)" />
+        <Line type="monotone" dataKey="Hamburg" stroke="hsla(112, 100%, 50%, 1)" />
+        <Line type="monotone" dataKey="Hessen" stroke="hsla(135, 100%, 50%, 1)" />
+        <Line type="monotone" dataKey="Mecklenburg-Vorpommern" stroke="hsla(157, 100%, 50%, 1)" />
+        <Line type="monotone" dataKey="Niedersachsen" stroke="hsla(180, 100%, 50%, 1)" />
+        <Line type="monotone" dataKey="Nordrhein-Westfalen" stroke="hsla(202, 100%, 50%, 1)" />
+        <Line type="monotone" dataKey="Rheinland-Pfalz" stroke="hsla(225, 100%, 50%, 1)" />
+        <Line type="monotone" dataKey="Saarland" stroke="hsla(247, 100%, 50%, 1)" />
+        <Line type="monotone" dataKey="Sachsen" stroke="hsla(270, 100%, 50%, 1)" />
+        <Line type="monotone" dataKey="Sachsen-Anhalt" stroke="hsla(292, 100%, 50%, 1)" />
+        <Line type="monotone" dataKey="Schleswig-Holstein" stroke="hsla(315, 100%, 50%, 1)" />
+        <Line type="monotone" dataKey="Thüringen" stroke="hsla(337, 100%, 50%, 1)" />
       </LineChart>
     </div>
   );
