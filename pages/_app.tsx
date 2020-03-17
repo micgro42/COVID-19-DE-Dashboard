fcba7e6836e2d6f0f82884900ea8b1d948b164fc
Michael Große <mic.grosse@googlemail.com>
2020-03-17 09:27:28 +0100

    Basic redux bootstrapping

---
+++ pages/_app.tsx
@@ -0,0 +1,28 @@
+import App from "next/app";
+import { Provider } from "react-redux";
+import React from 'react';
+import store from "../redux/store";
+
+class MyApp extends App {
+  static async getInitialProps({ Component, ctx }: any) { // TODO: remove that any
+    const pageProps = Component.getInitialProps
+      ? await Component.getInitialProps(ctx)
+      : {};
+
+    //Anything returned here can be access by the client
+    return { pageProps: pageProps };
+  }
+
+  render() {
+    //Information that was returned  from 'getInitialProps' are stored in the props i.e. pageProps
+    const {Component, pageProps} = this.props;
+
+    return (
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    );
+  }
+}
+
+export default MyApp;
